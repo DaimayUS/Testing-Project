d2a0aa746cde513b405b9e40f4ed2fff1ce694ce
DaimayUS <60114342+DaimayUS@users.noreply.github.com>
2020-01-24 14:46:46 -0500

    Update misra_c.c

--- misra_c.c
+++ misra_c.c
@@ -1379,7 +1379,7 @@ void GM_MISRAC_2012_R18_6(void)
 /* Rule 18.8    Variable-length array types shall not be used  */
 /***************************************************************/
 #ifndef _MSC_VER
-void GM_MISRA_2012_R18_8(int16_t n, uint16_t a[10][n])  /* Non-compliant */
+void GM_MISRA_2012_R18_8(int16_t n, uint16_t a[10][n]);  /* Non-compliant */
 {
     n = -1;
     uint16_t vla [10][n];               /* Non-compliant - Undefined if n <=0 */
